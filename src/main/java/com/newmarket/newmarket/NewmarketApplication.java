7509ff96c1e5e1fd25d6ba0d74b7c03e86b599af
BrightHee <jjay3518@gmail.com>
2020-09-11 16:11:36 +0900

    Initial commit

---
+++ src/main/java/com/newmarket/newmarket/NewmarketApplication.java
@@ -0,0 +1,13 @@
+package com.newmarket.newmarket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NewmarketApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NewmarketApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/newmarket/newmarket/NewmarketApplicationTests.java
@@ -0,0 +1,13 @@
+package com.newmarket.newmarket;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NewmarketApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
